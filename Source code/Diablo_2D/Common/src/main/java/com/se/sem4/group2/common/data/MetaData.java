c117b9fda3e3d1fe34b4a6b87b9b8397294a1ce9
Thomas Lemqvist <ThomasLemqvist@DrBumlehund.local>
2016-02-29 14:24:23 +0100

    Added MetaData.java, Entity.java, EntityType.java, and services.

--- Source code/Diablo_2D/Common/src/main/java/com/se/sem4/group2/common/data/MetaData.java
+++ Source code/Diablo_2D/Common/src/main/java/com/se/sem4/group2/common/data/MetaData.java
@@ -10,5 +10,33 @@ package com.se.sem4.group2.common.data;
  * @author ThomasLemqvist
  */
 public class MetaData {
-    
+
+    private float deltaTime;
+    private int width;
+    private int height;
+
+    public float getDeltaTime() {
+        return deltaTime;
+    }
+
+    public void setDeltaTime(float deltaTime) {
+        this.deltaTime = deltaTime;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
 }
