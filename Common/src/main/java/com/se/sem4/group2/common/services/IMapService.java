539891e0a7bf01046d204f973d71e22d57086aea
Thomas Lemqvist <ThomasLemqvist@DrBumlehund.local>
2016-03-04 10:29:21 +0100

    moved report to own repo

---
+++ Common/src/main/java/com/se/sem4/group2/common/services/IMapService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.se.sem4.group2.common.services;
+
+/**
+ *
+ * @author ThomasLemqvist
+ */
+public interface IMapService {
+    
+}
